8c8442018a839d1323c82b5dba51ec692733fe27
Gilbert Kwan <gkwan@ca.ibm.com>
2023-05-11 09:11:43 -0400

    Update SystemEndpointIT.java
    update as same as finish

--- start/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
+++ start/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -37,12 +37,12 @@ public class SystemEndpointIT {
     private static Client client;
 
     @BeforeAll
-    private static void setup() {
+    public static void setup() {
         client = ClientBuilder.newClient();
     }
 
     @AfterAll
-    private static void teardown() {
+    public static void teardown() {
         client.close();
     }
 
