60dd2f7a95cd1378e362844ddd6bd85043104455
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-10 15:11:19 -0400

    Skip features.sh in docker test scripts

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -14,6 +14,7 @@ sed -i "\#<looseApplication>false</looseApplication>#a<install><runtimeUrl>https
 cat query/pom.xml graphql/pom.xml system/pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-..JAVA_VERSION.-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
 cat system/Dockerfile graphql/Dockerfile query/Dockerfile
 
 docker pull "$DOCKER_USERNAME""/olguides:""$BUILD"
--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -15,6 +15,7 @@ sed -i "\#<looseApplication>false</looseApplication>#a<install><runtimeUrl>https
 cat query/pom.xml graphql/pom.xml system/pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-..JAVA_VERSION.-openj9-ubi;FROM openliberty/daily:full-java11-openj9;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
 cat system/Dockerfile graphql/Dockerfile query/Dockerfile
 
 docker pull "openliberty/daily:latest"
