0e884777009eb2ba45740f1a2dc14a0b3b5ac7dc
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-22 09:23:29 -0500

    Update dockerImageTest.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -14,7 +14,7 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install
 sed -i "\#<looseApplication>false</looseApplication>#a<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>"  graphql/pom.xml
 cat query/pom.xml graphql/pom.xml system/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-..JAVA_VERSION.-openj9-ubi;FROM openliberty/daily:full-java11-openj9;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-..JAVA_VERSION.-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
 sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile graphql/Dockerfile query/Dockerfile
 cat system/Dockerfile graphql/Dockerfile query/Dockerfile
 
