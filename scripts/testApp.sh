94173b8b5c86b46ab6558857eda1a8ceb5f6ddca
Andrew Sasmito <Andrew.Sasmito@ibm.com>
2025-05-16 13:16:30 -0400

    Update to use maven wrapper
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 ./scripts/packageApps.sh
 
-mvn -ntp -pl system liberty:create liberty:install-feature liberty:deploy
-mvn -ntp -pl graphql liberty:create liberty:install-feature liberty:deploy
+./mvnw -ntp -pl system liberty:create liberty:install-feature liberty:deploy
+./mvnw -ntp -pl graphql liberty:create liberty:install-feature liberty:deploy
 
-mvn -ntp -pl system liberty:start
-mvn -ntp -pl graphql liberty:start
+./mvnw -ntp -pl system liberty:start
+./mvnw -ntp -pl graphql liberty:start
 
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl system failsafe:integration-test
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -pl graphql failsafe:integration-test
-mvn -ntp -pl system failsafe:verify
-mvn -ntp -pl graphql failsafe:verify
+./mvnw -ntp -pl system failsafe:verify
+./mvnw -ntp -pl graphql failsafe:verify
 
-mvn -ntp -pl system liberty:stop
-mvn -ntp -pl graphql liberty:stop
+./mvnw -ntp -pl system liberty:stop
+./mvnw -ntp -pl graphql liberty:stop
 
-mvn -ntp -pl query liberty:create liberty:install-feature liberty:deploy
+./mvnw -ntp -pl query liberty:create liberty:install-feature liberty:deploy
 
 docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
 
@@ -32,4 +33,4 @@ docker build -t system:1.0-java17-SNAPSHOT --build-arg JAVA_VERSION=java17 syste
 docker build -t graphql:1.0-SNAPSHOT graphql/.
 docker build -t query:1.0-SNAPSHOT query/.
 
-mvn -ntp -pl query verify
+./mvnw -ntp -pl query verify
